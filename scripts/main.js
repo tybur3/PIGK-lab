4c33c787142cad1e6a82aaf25eb7a0608b71c70b
Piotr T <ptyburczy@gmail.com>
2021-03-12 14:15:50 +0100

    Zad 1 - 4 done

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,10 @@
-$(function () {
-  $('#accordion').accordion();
-});
+// $(function () {
+//   $('#accordion').accordion();
+// });
 
-$(function () {
-  $('#tabs').tabs();
-});
+// $(function () {
+//   $('#tabs').tabs();
+// });
 
 //zad7
 $(function () {
@@ -145,3 +145,125 @@ $(function () {
     $('#dialog-form').dialog('open');
   });
 });
+
+//Lab3
+
+//zad1
+
+$(document).ready(() => {
+  $('#accordion').accordion({
+    animate: {
+      // duration: 500,
+      // easing: 'easeOutBounce',
+      down: {
+        easing: 'easeOutBounce',
+        duration: 1000,
+      },
+    },
+  });
+});
+
+//zad2
+
+$(function () {
+  $('#tabs').tabs({
+    event: 'mouseover',
+    show: { effect: 'drop', duration: 200 },
+    // hide: { effect: 'explode', duration: 1000 },
+  });
+});
+
+// $('#add_tab')
+//   .button()
+//   .on('click', function () {
+//     dialog.dialog('open');
+//   });
+
+$(function () {
+  var tabTitle = $('#tab_title'),
+    tabContent = $('#tab_content'),
+    tabTemplate =
+      //"<li><a href='#{href}'>#{label}</a> <span class='ui-icon ui-icon-close' role='presentation'>Remove Tab</span></li>",
+      `<li><a href='#{href}'>#{label}</a></li>`,
+    tabCounter = 3;
+
+  var tabs = $('#tabs').tabs();
+
+  // Modal dialog init: custom buttons and a "close" callback resetting the form inside
+  var dialog = $('#dialog-tab').dialog({
+    autoOpen: false,
+    modal: true,
+    buttons: {
+      Add: function () {
+        addTab();
+        $(this).dialog('close');
+      },
+      Cancel: function () {
+        $(this).dialog('close');
+      },
+    },
+    close: function () {
+      form[0].reset();
+    },
+  });
+
+  // AddTab form: calls addTab function on submit and closes the dialog
+  var form = dialog.find('form').on('submit', function (event) {
+    addTab();
+    dialog.dialog('close');
+    event.preventDefault();
+  });
+
+  // Actual addTab function: adds new tab using the input from the form above
+  function addTab() {
+    var label = tabTitle.val() || 'Tab ' + tabCounter,
+      id = 'tabs-' + tabCounter,
+      li = $(
+        tabTemplate
+          .replace(/#\{href\}/g, '#' + id)
+          .replace(/#\{label\}/g, label)
+      ),
+      tabContentHtml = tabContent.val() || 'Tab ' + tabCounter + ' content.';
+
+    tabs.find('.ui-tabs-nav').append(li);
+    tabs.append("<div id='" + id + "'><p>" + tabContentHtml + '</p></div>');
+    tabs.tabs('refresh');
+    tabCounter++;
+  }
+
+  // AddTab button: just opens the dialog
+  $('#add_tab')
+    .button()
+    .on('click', function () {
+      dialog.dialog('open');
+    });
+
+  // Close icon: removing the tab on click
+  // tabs.on('click', 'span.ui-icon-close', function () {
+  //   var panelId = $(this).closest('li').remove().attr('aria-controls');
+  //   $('#' + panelId).remove();
+  //   tabs.tabs('refresh');
+  // });
+
+  // tabs.on('keyup', function (event) {
+  //   if (event.altKey && event.keyCode === $.ui.keyCode.BACKSPACE) {
+  //     var panelId = tabs.find('.ui-tabs-active').remove().attr('aria-controls');
+  //     $('#' + panelId).remove();
+  //     tabs.tabs('refresh');
+  //   }
+  // });
+});
+
+//zad4
+
+$(function () {
+  var tabs = $('#tabs').tabs();
+  tabs.find('.ui-tabs-nav').sortable({
+    axis: 'x',
+    stop: function () {
+      tabs.tabs('refresh');
+    },
+  });
+});
+
+//zad5
